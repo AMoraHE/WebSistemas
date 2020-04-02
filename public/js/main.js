22f4800dff2ab3d8ad51d4b2fe7d0d673b9927f6
WesterlyJoker <Carlozbe69@gmail.com>
2020-04-01 19:43:40 -0500

    Actualizacion 69 :v

--- public/js/main.js
+++ public/js/main.js
@@ -40,4 +40,20 @@ $(document).ready(function(){
 
 	});**/
 
+	/*********************************************************Boton Ir Arriba**************************************************************/
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+
 });
