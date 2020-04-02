2885a2781cb9ff100f55532502a81bb9bebfdbb3
Slender5 <44220732+Slender5@users.noreply.github.com>
2020-04-02 18:04:02 -0500

    Slender5
    Holi bb

--- resources/views/admin/menu-inf/formEdit.blade.php
+++ resources/views/admin/menu-inf/formEdit.blade.php
@@ -1,112 +1,72 @@
-<div>
+
+<form class="form-group" files = "true" method="POST" action="/Lab-Celula/{{$infra->slug}}" enctype="multipart/form-data">
+  
+  @method('PUT')
   @csrf
-    <div class="formulario">
+  
+    <div>
 
-          <label for="">Tamaño de imagen recomendado: 1100px de ancho y 450px de alto</label>
-          
+      <div class="formulario">
+        <label for="">Tamaño de imagen recomendado: 1100px de ancho y 450px de alto</label>        
       </div>
-
 
       <div class="contenedor-cargar-img">
 
-          <div class="contenedor-txt-seccion">
+        <div class="contenedor-txt-seccion">
+          <label for="">Cargar IMG</label>
+        </div>
 
-            <label for="">Cargar IMG</label>
+        <div class="contenedor-boton-cargar">
+          <label for="file-upload" class="subir">
+            <i class="fas fa-cloud-upload-alt"></i> Subir Archivo
+          </label>
 
-          </div>
+          <input id="file-upload" name="imgInicio" value="/img/slider/" onchange='cambiar()' type="file" accept="image/gif, image/jpeg, image/png" style='display: none;' required />
 
-          <div class="contenedor-boton-cargar">
-
-            <label for="file-upload" class="subir">
-              <i class="fas fa-cloud-upload-alt"></i> Subir Archivo
-            </label>
-
-            <input id="file-upload" name="imgInicio" value="/img/slider/" onchange='cambiar()' type="file" accept="image/gif, image/jpeg, image/png" style='display: none;' required />
-
-            <div id="info-img">No se Eligió Archivo</div>
-
-
-          </div>
-
-
-          <div class="contenedor-img-seccion">
-
-            <img id="img-pre" src="/img/LogoNuevaImagen.png">
-
-          </div>
-
+          <div id="info-img">No se Eligió Archivo</div>
 
       </div>
 
-      <!----------------------------------------------------------------- Información ---------------------------------------------------------->
-    
 
-    <div class="formulario">
+      <div class="contenedor-img-seccion">
+        <img id="img-pre" src="/img/LogoNuevaImagen.png">
+      </div>
+
+
+    </div>
+
+        <!----------------------------------------------------------------- Información ---------------------------------------------------------->
       
-      <label for="">Responsable</label>
-      <input type="text" name="responsable" id ="" value="{{$infra->responsable}}"  class="form-control" required>
 
-    </div>
+      <div class="formulario">
+        
+        <label for="">Responsable</label>
+        <input type="text" name="responsable" id ="" value="{{$infra->responsable}}"  class="form-control" required>
 
-    <div class="formulario">
+      </div>
 
-      <label for="">Correo Electrónico</label>
-      <input type="email" name="correo" id="correo" value="{{$infra->correo}}" required>
+      <div class="formulario">
 
-    </div>
+        <label for="">Correo Electrónico</label>
+        <input type="email" name="correo" id="correo" value="{{$infra->correo}}" required>
+
+      </div>
 
 
-    <div class="formulario">
+      <div class="formulario">
 
-      <label for="">Descripción:</label>
-      <textarea name="descripcion" placeholder="Inserte Descripción del Laboratorio ... " required>{{$infra->responsable}}</textarea>
+        <label for="">Descripción:</label>
+        <textarea name="descripcion" placeholder="Inserte Descripción del Laboratorio ... " required>{{$infra->responsable}}</textarea>
 
-    </div>
+      </div>
+      
+    <div class="contenedor-img-seccion">
     
-  <div class="contenedor-img-seccion">
-    <table>
-      <tr>
-        <td>
-          <label>Imagen 1</label>
-          <input type="file" name="img1">
-        </td>
-        <td><img id="img-pre" src="/img/LogoNuevaImagen.png"></td>
-      </tr>
-      <tr>
-        <td>
-          <label>Imagen 2</label>
-          <input type="file" name="img2">
-        </td>
-        <td><img id="img-pre" src="/img/LogoNuevaImagen.png"></td>
-      </tr>
-      <tr>
-        <td>
-          <label>Imagen 3</label>
-          <input type="file" name="img3">
-        </td>
-        <td><img id="img-pre" src="/img/LogoNuevaImagen.png"></td>
-      </tr>
-      <tr>
-        <td>
-          <label>Imagen 4</label>
-          <input type="file" name="img4">
-        </td>
-        <td><img id="img-pre" src="/img/LogoNuevaImagen.png"></td>
-      </tr>
-      <tr>
-        <td>
-          <label>Imagen 5</label>
-          <input type="file" name="img5">
-        </td>
-        <td><img id="img-pre" src="/img/LogoNuevaImagen.png"></td>
-      </tr>
-      <tr>
-        <td>
-          <label>Imagen 6</label>
-          <input type="file" name="img6">
-        </td>
-        <td><img id="img-pre" src="/img/LogoNuevaImagen.png"></td>
-      </tr>
-    </table>
+    </div>
+
+    <div class="contenedor-botones">     
+        <button type="submit" class="btn"><span>Guardar</span></button>
+        <a onclick="return confirm('¿Desea bandonar sin guardar?')" href="{{url()->previous()}}"><button type="button" class="btn"><span>Cancelar</span></button></a>
+    </div>
   </div>
-</div>
+</form>
