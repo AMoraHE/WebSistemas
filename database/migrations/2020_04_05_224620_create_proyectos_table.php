d2c63361140c73f02e7d3ba7cf4d6a644f108e51
OssielFabianCanulMis <ossielcanulmis1998@gmail.com>
2020-04-06 05:08:52 -0500

    :v

---
+++ database/migrations/2020_04_05_224620_create_proyectos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProyectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('aplicacion')->unique();
+            $table->String('proyecto');
+            $table->String('desarrolladores');
+            $table->String('objetivo');
+            $table->String('descripcion');
+            $table->String('resultados');
+            $table->String('newimage');
+            $table->string('slug')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyectos');
+    }
+}
