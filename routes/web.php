10f37bfefa49e6fc0d19ac02980bf7111398da0f
AMoraHE <55561577+AMoraHE@users.noreply.github.com>
2020-04-11 17:13:16 -0500

    Revert "Slender"
    This reverts commit c13db6afcc6cdfef7c29436c194682f1cbf847c3.

--- routes/web.php
+++ routes/web.php
@@ -223,9 +223,3 @@ Route::get('/Album/{slug}/edit', 'AlbumController@edit')->middleware('auth');
 Route::get('/Album/agregar/{slug}', 'AlbumController@agregar')->middleware('auth');
 Route::get('/Album-Delete/{slug}', 'AlbumController@destroy')->middleware('auth');
 Route::get('/Album-Galeria-Delete/{id}', 'AlbumGaleriaController@destroy')->middleware('auth');
-
-
-
-
-
-Route::resource('Test', 'TestPublicController')->middleware('auth');;
