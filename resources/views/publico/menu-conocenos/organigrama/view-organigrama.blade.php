2ebdee8397982edab12a18eb13d0177348314294
Roiser121 <55642362+Roiser121@users.noreply.github.com>
2020-04-23 16:42:34 -0500

    Organigrama public V1
    Organigrama con bugs :´v

--- resources/views/publico/menu-conocenos/organigrama/view-organigrama.blade.php
+++ resources/views/publico/menu-conocenos/organigrama/view-organigrama.blade.php
@@ -25,13 +25,64 @@
 
 
 <!------------------------------------------------------------------ VISTA-ORGANIGRAMA ------------------------------------------------------------->
-    <div class="chart" id="organigrama"> --@-- </div>
 
-    <script>
-        new Treant( chart_config );
-    </script>
-      
+@foreach ($organigramas as $organigrama)
+    <div class="contenedor-organigrama">
+        <div class="contenedor-dir"><!------------------- CONTENEDOR DIR/SUB/DIV ----------------------------->
+                <div class="contenedor-inf">
+                    <a href="#">
+                        <label>
+                        <p id="titulo-organigrama">Dirección General</p>
+                        <img src="img/1.jpg">
+                        <p>Diego Briseño</p>
+                        </label>
+                    </a>
+                            
+                </div>
+            <div class="contenedor-lab-doc"><!------------------- CONTENEDOR LAB/DOCENTES ----------------------------->
 
+                <div class="titulo-org1">
+                    <p id="titulo-organigrama">Laboratorios</p>
+                </div>
+
+                <div class="titulo-org2">
+                    <p id="titulo-organigrama">Docentes</p>
+                            
+                </div>
+
+                    <div class="contenedor-lab"><!------------------- CONTENEDOR LABORATORIOS ----------------------------->
+
+                        <div class="contenedor-inf">
+                    <a href="#">
+                        <label>
+                        <p id="titulo-organigrama">Laboratorio de Sistemas Computacionales</p>
+                        <img src="img/1.jpg">
+                        <p>Diego Briseño</p>
+                        </label>
+                    </a>
+                            
+                        </div>  
+                    </div>
+
+                    <div class="contenedor-doc"> <!------------------- CONTENEDOR DOCENTES ----------------------------->
+
+                    <div class="contenedor-inf">
+                    <a href="#">
+                        <label>
+                        <p id="titulo-organigrama">Docente</p>
+                        <img src="img/1.jpg">
+                        <p>MC. Arizbé del Socorro Arana Kantún</p>
+                        </label>
+                    </a>
+                            
+                    </div>
+
+                    </div>   
+            </div>
+        </div>
+    </div>
+
+    @endforeach
 </div>
 
 @endsection
