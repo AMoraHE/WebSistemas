8315829f4408f1bc7bdcc0adcaa8f201ee84f35a
WesterlyJoker <Carlozbe69@gmail.com>
2020-05-11 16:22:50 -0500

    proyectos

--- resources/views/publico/menu-academicos/proyectos/view-proyectos-academicos.blade.php
+++ resources/views/publico/menu-academicos/proyectos/view-proyectos-academicos.blade.php
@@ -9,104 +9,49 @@
 
 <div class="seccion-principal">
 
-	<!-----------------Botones--------->
+	<!------------------------------------------------------------- IMG Encabezado ----------------------------------------------------------->
 
+      <div class="contenedor-img-encabezado">
+
+          <div class="img-encabezado">
+
+            <img src="/img/Convocatorias.jpg">
+
+            <div class="encabezado">
+
+               <h1>Proyectos</h1>
+               <hr>
+
+            </div>
+
+          </div>   
+
+          <div class="skew-abajo"></div>
+
+      </div>
+
+    <div class="contenedor-secciones">
 
 	@foreach ($proyectos as $proyecto)
 
 	<div class="contenedor-titulo-seccion">
 
-		<h3>{{$proyecto->proyecto}}</h3>
+			<h3><i class="icono-izquierda fas fa-file-invoice"></i>{{$proyecto->proyecto}}</h3>
 
 	</div>
 
-	<div class="contenedor-img-grande">
+	<div class="contenedor-seccion-img-grande">
 
-		<a target="_blank" rel="noopener noreferrer" href="/isc-academicos-proyectosVerImg/{{$proyecto->id}}"><img src="/images/proyectos/{{$proyecto->newimage}}"></a>
-
-	</div>
-
-	<!----------------------------------------------------------------- DIVISOR ---------------------------------------------------------->
-
-	<div class="divisor-contenido"></div>
-
-	<!----------------------------------------------------------------- TABLA ---------------------------------------------------------->
-
-
-			<div class="tabla-informacion-escolar">
-
-				<table>
-
-					<thead>
-						<tr>
-							<th>Desarrolladores</th>
-							<th>Descripción</th><!--Comentar-->
-						</tr>
-					</thead>
-
-						<tr>
-							<td>
-
-							<ol>
-								@foreach (explode(', ', $proyecto->desarrolladores) as $desarrolladores)
-								<li>{{$desarrolladores}}</li>
-								@endforeach
-							</ul>
-
-							</td>
-
-							<td>{{$proyecto->descripcion}}</td><!--Comentar-->
-							<!----<td>{{$proyecto->resultados}}</td>---->
-						</tr>
-
-<!--					<thead>
-						<tr>
-							<th colspan="4">Descripción</th>
-						</tr>
-					</thead>
-
-						<tr>
-							<td colspan="4">{{$proyecto->descripcion}}</td>
-						</tr>	-->
-
-				</table>
-
-			</div>
-
-
-<!----------------------------------------------------------------- GALERÍA ---------------------------------------------------------->
-	<div class="formulario">
-
-		<label>Mini Galería: </label>
+		<img src="/images/proyectos/{{$proyecto->newimage}}">
+		<div class="skew-abajo"></div>
 
 	</div>
 
-	<div class="seccion-mini-galeria">
-
-		@foreach ($imagenes as $imagen)
-		@if($imagen->proySlug == $proyecto ->slug)
-
-		<div class="contenedor-img-mini-galeria">
-
-			<div class="contenedor-img-mini-galeria-img">
-
-				<a target="_blank" rel="noopener noreferrer" href="/isc-academicos-proyectosGaleriaVerImg/{{$imagen->id}}"><img src="/images/proyectos/{{$imagen->imagen}}"></a>
-
-			</div>
-
-			
-		</div>
-
-		@endif
-		@endforeach
-
-	</div>
-
-<!---------------BOTONES PROYECTOS------------------------------------->
-
-
-
+	
+	<div class="seccion-divisor-seccion"></div>
 	@endforeach
+
+	</div>
 
 	<div id="paginas">
 			{{$proyectos->links()}}
