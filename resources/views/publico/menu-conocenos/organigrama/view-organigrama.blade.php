7708c991091c4aa8de56c7ecc633b2af97e2295d
Roiser121 <55642362+Roiser121@users.noreply.github.com>
2020-05-15 14:17:31 -0500

    Vista Organigrama V1

--- resources/views/publico/menu-conocenos/organigrama/view-organigrama.blade.php
+++ resources/views/publico/menu-conocenos/organigrama/view-organigrama.blade.php
@@ -17,76 +17,179 @@
 
 <div class="seccion-principal">
 
-  <div class="contenedor-titulo-seccion">
+   <div class="contenedor-img-encabezado">
 
-    <div class="tlinea"></div>
+          <div class="img-encabezado">
 
-    <h3>Organigrama</h3>
+            <img src="/img/Conocenos.jpg">
 
-    <div class="tlinea"></div>
+            <div class="encabezado">
 
-  </div>
+               <h1>Organigrama</h1>
+               <hr>
+
+            </div>
+
+          </div>   
+
+          <div class="skew-abajo-org"></div>
+
+      </div>
 
 
 <!------------------------------------------------------------------ VISTA-ORGANIGRAMA ------------------------------------------------------------->
     <div class="contenedor-organigrama">
-        <div class="contenedor-dir"><!------------------- CONTENEDOR DIR/SUB/DIV ----------------------------->
+        <div class="contenedor-dir"><!------------------- CONTENEDOR DIR ----------------------------->
                 @foreach($director as $direc)
-                <div class="contenedor-inf">
-                    <a href="/isc-inicio-formContacto/{{$direc->correo}}" onclick="return confirm('¿Contactar?')">
-                        <label>
-                            <p id="titulo-organigrama">{{$direc->nombre}}</p>
-                            <img src="/images/organigrama/{{$direc->foto}}">
-                            <p>{{$direc->puesto}}</p>
-                            <p>{{$direc->integrante}}</p>
-                            <p>{{$direc->correo}}</p>
-                        </label>
-                    </a>
+
+                @if($direc->puesto == 'Dirección General')
+                <div class="contenedor-inf-dir">
+                    
+                    <div class="contenedor_tarjeta">
+                        <a href="/isc-inicio-formContacto/{{$direc->correo}}" onclick="return confirm('¿Contactar?')">
+                            <figure id="tarjeta">
+                                <img src="/images/organigrama/{{$direc->foto}}" class="frontal" alt="">
+                                <figcaption class="trasera">
+                                    <h2 class="titulo">{{$direc->nombre}}</h2>
+                                    <hr>
+                                        <p>{{$direc->puesto}}</p>
+                                        <p>{{$direc->integrante}}</p>
+                                        <p>{{$direc->correo}}</p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
                             
                 </div>
+
+                @endif  
                 @endforeach
+
+
+<!------------------- CONTENEDOR /SUB/DIV ----------------------------->
+                 <div class="contenedor-inf-dir">
+                
+                 @foreach($director as $direc)
+
+
+                    @if($direc->puesto == 'Dirección Académica y de vinculación')
+                    <div class="contenedor_tarjeta">
+                        <a href="/isc-inicio-formContacto/{{$direc->correo}}" onclick="return confirm('¿Contactar?')">
+                            <figure id="tarjeta">
+                                <img src="/images/organigrama/{{$direc->foto}}" class="frontal" alt="">
+                                <figcaption class="trasera">
+                                    <h2 class="titulo">{{$direc->nombre}}</h2>
+                                    <hr>
+                                        <p>{{$direc->puesto}}</p>
+                                        <p>{{$direc->integrante}}</p>
+                                        <p>{{$direc->correo}}</p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+                    @endif
+
+                     @if($direc->puesto == 'Subdirección Académica')
+                    <div class="contenedor_tarjeta">
+                        <a href="/isc-inicio-formContacto/{{$direc->correo}}" onclick="return confirm('¿Contactar?')">
+                            <figure id="tarjeta">
+                                <img src="/images/organigrama/{{$direc->foto}}" class="frontal" alt="">
+                                <figcaption class="trasera">
+                                    <h2 class="titulo">{{$direc->nombre}}</h2>
+                                    <hr>
+                                        <p>{{$direc->puesto}}</p>
+                                        <p>{{$direc->integrante}}</p>
+                                        <p>{{$direc->correo}}</p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+                    @endif
+
+                     @if($direc->puesto == 'División de ISC')
+                    <div class="contenedor_tarjeta">
+                        <a href="/isc-inicio-formContacto/{{$direc->correo}}" onclick="return confirm('¿Contactar?')">
+                            <figure id="tarjeta">
+                                <img src="/images/organigrama/{{$direc->foto}}" class="frontal" alt="">
+                                <figcaption class="trasera">
+                                    <h2 class="titulo">{{$direc->nombre}}</h2>
+                                    <hr>
+                                        <p>{{$direc->puesto}}</p>
+                                        <p>{{$direc->integrante}}</p>
+                                        <p>{{$direc->correo}}</p>
+                                </figcaption>
+                            </figure>
+                        </a>
+                    </div>
+                    @endif
+                            
+                
+  
+                @endforeach
+                </div>
+
             <div class="contenedor-lab-doc"><!------------------- CONTENEDOR LAB/DOCENTES ----------------------------->
 
                 <div class="titulo-org1">
                     <p id="titulo-organigrama">Laboratorios</p>
+                    <hr>
                 </div>
 
                 <div class="titulo-org2">
                     <p id="titulo-organigrama">Docentes</p>
+                    <hr>
                             
                 </div>
 
                     <div class="contenedor-lab"><!------------------- CONTENEDOR LABORATORIOS ----------------------------->
 
+                    <div class="contenedor-inf-lab">
                         @foreach($laboratorio as $lab)
-                        <div class="contenedor-inf">
-                            <a href="/isc-inicio-formContacto/{{$lab->correo}}" onclick="return confirm('¿Contactar?')">
-                                <label>
-                                    <img src="/images/organigrama/{{$lab->foto}}">
-                                    <p>{{$lab->puesto}}</p>
-                                    <p>{{$lab->integrante}}</p>
-                                    <p>{{$lab->correo}}</p>
-                                </label>
-                            </a>
-                        </div>  
+                       
+                            
+                            <div class="contenedor_tarjeta">
+                                <a href="/isc-inicio-formContacto/{{$lab->correo}}" onclick="return confirm('¿Contactar?')">
+                                    <figure id="tarjeta">
+                                        <img src="/images/organigrama/{{$lab->foto}}" class="frontal" alt="">
+                                        <figcaption class="trasera">
+                                            <h2 class="titulo">{{$lab->puesto}}</h2>
+                                            <hr>
+                                                <p>{{$lab->integrante}}</p>
+                                                <p>{{$lab->correo}}</p>
+                                        </figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                          
                         @endforeach
+                    </div>
 
                     </div>
 
                     <div class="contenedor-doc"> <!------------------- CONTENEDOR DOCENTES ----------------------------->
 
+
+                     <div class="contenedor-inf-doc">
+
                         @foreach($docente as $docen)
-                        <div class="contenedor-inf">
-                            <a href="/isc-inicio-formContacto/{{$docen->correo}}" onclick="return confirm('¿Contactar?')">
-                                <label>
-                                    <img src="/images/organigrama/{{$docen->foto}}">
-                                    <p>{{$docen->puesto}}</p>
-                                    <p>{{$docen->integrante}}</p>
-                                    <p>{{$docen->correo}}</p>
-                                </label>
-                            </a>
-                        </div>
+
+                            <div class="contenedor_tarjeta">
+                                <a href="/isc-inicio-formContacto/{{$docen->correo}}" onclick="return confirm('¿Contactar?')">
+                                    <figure id="tarjeta">
+                                        <img src="/images/organigrama/{{$docen->foto}}" class="frontal" alt="">
+                                        <figcaption class="trasera">
+                                            <h2 class="titulo">{{$docen->puesto}}</h2>
+                                            <hr>
+                                                <p>{{$docen->integrante}}</p>
+                                                <p>{{$docen->correo}}</p>
+                                        </figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+
+                        
                         @endforeach
+                    </div>
 
                     </div>   
             </div>
