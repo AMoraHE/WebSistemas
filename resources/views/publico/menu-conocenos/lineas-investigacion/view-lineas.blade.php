dd9c3c3911bde9cd834e68a8a3563e580ede104f
Roiser121 <55642362+Roiser121@users.noreply.github.com>
2020-05-17 12:37:33 -0500

    Agrupar lineas de investigacion con cuerpos Acádemicos
    agregado el recorte de imagenes en organigrama

--- resources/views/publico/menu-conocenos/lineas-investigacion/view-lineas.blade.php
+++ resources/views/publico/menu-conocenos/lineas-investigacion/view-lineas.blade.php
@@ -6,19 +6,33 @@
 
 <div class="seccion-principal">
 
+	<div class="contenedor-img-encabezado">
+
+          <div class="img-encabezado">
+
+            <img src="/img/Conocenos.jpg">
+
+            <div class="encabezado">
+
+               <h1>Líneas y Cuerpos</h1>
+               <hr>
+
+            </div>
+
+          </div>   
+
+          <div class="skew-abajo"></div>
+
+      </div>
+
 <!----------------------------------------------------------------- TABLA LINEAS INVESTIGACIÓN---------------------------------------->
 
-	<div class="contenedor-titulo-seccion">
-
-		<div class="tlinea"></div>
-
-		<h3>Líneas de Investigación</h3>
-
-		<div class="tlinea"></div>
-
-	</div>
-
 	<div class="contenedor-tabla">
+
+		<div class="titulo-linea-cuerpo">
+		 <p>Líneas de investigación</p>
+		</div>
+
 
 		<div class="tabla-informacion-escolar">
 
@@ -47,6 +61,51 @@
 
 		</div>
 	</div>
+<!----------------------------------------------------------------- TABLA CUERPOS ACÁDEMICOS---------------------------------------->
+	<div class="contenedor-tabla">
+
+		<div class="titulo-linea-cuerpo">
+		 <p>Cuerpos Académicos</p>
+		</div>
+
+			<div class="tabla-informacion-escolar">
+
+				<table>
+							
+					<thead>
+						<tr>
+							<th>Nombre del CA</th>
+							<th>Grado del CA</th>
+							<th>ID CA</th>
+							<th>Clave CA</th>
+							<th>Integrantes</th>
+						</tr>
+					</thead>
+
+					@foreach($cuerpos as $cuerpo)
+
+					<tr>
+						<td>{{$cuerpo->nombre}}</td>
+						<td>{{$cuerpo->grado}}</td>
+						<td>{{$cuerpo->idCA}}</td>
+						<td>{{$cuerpo->clave}}</td>
+						<td>
+							<ol>
+								@foreach (explode(', ', $cuerpo->integrantes) as $integrantes)
+								<li>{{$integrantes}}</li>
+								@endforeach
+							</ol>
+						</td>
+
+					</tr>
+
+					@endforeach
+
+				</table>		
+
+			</div>
+		</div>
+		
 				
 </div>
 
